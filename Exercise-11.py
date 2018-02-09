3b0b59998e39bd381dae06c0318cedf90bbd74bf
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-09 20:51:11 +0100

    add Exercise 11, add defination of tuple

--- Exercise-11.py
+++ Exercise-11.py
@@ -1,8 +1,16 @@
 import numpy as np
+import time
 
 # Convert a list and tuple into arrays
 my_list=[1,2,3,4,5]
 x=np.asarray(my_list)
 print(x)
 
-my_tuple=
+#defination of tuple
+#A tuple is a sequence of immutable Python objects. Tuples are sequences, just like lists. The differences between tuples and lists are, the tuples cannot be changed unlike lists and tuples use parentheses, whereas lists use square brackets.
+#Creating a tuple is as simple as putting different comma-separated values.
+print(time.localtime())
+print(range(10))
+
+my_tuple=([1,1,1],[2,3,3])
+print(my_tuple)
