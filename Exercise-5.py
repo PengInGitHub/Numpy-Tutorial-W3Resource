d103c121c12f9160c45d70d282ae9bea054b3b0c
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-09 18:00:11 +0100

    add Exercise 5 create array, np.arange(12,28)

---
+++ Exercise-5.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+#Create a array with values ranging from 12 to 38
+#Write a Python program to create a array with values ranging from 12 to 38.
+x=np.arange(12,28)
+print(type(x),x)
