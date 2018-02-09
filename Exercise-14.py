632e00080d24e59ee96523afb93ac98e1d158905
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-09 21:49:11 +0100

    add Exercise 14 operations directly on np.array object

---
+++ Exercise-14.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+#Write a Python program to convert the values of Centigrade degrees into Fahrenheit degrees. Centigrade values are stored into a NumPy array.
+Fahrenheit=[0, 12, 45.21, 34, 99.91]
+FArray=np.array(Fahrenheit)
+print(5*FArray/9 - 5*32/9)
